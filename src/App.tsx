702e585ab32dd2121b04de5e342a29a6f0a1ab44
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-12-22 13:07:52 +0600

    added Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import AllProducts from "./components/AllProducts";
 import "./App.css";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <AllProducts />
       <Footer />
+      <Login />
     </div>
   );
 }
